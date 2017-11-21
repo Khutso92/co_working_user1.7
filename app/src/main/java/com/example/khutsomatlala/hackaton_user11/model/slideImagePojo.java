8c50bf96f2e5fa37bd65ad735e1e5ebfc3908a78
Lerato Sambula <sambula.lerato4@gmail.com>
2017-11-21 13:44:20 +0200

    image slider and auth ui

---
+++ app/src/main/java/com/example/khutsomatlala/hackaton_user11/model/slideImagePojo.java
@@ -0,0 +1,21 @@
+package com.example.khutsomatlala.hackaton_user11.model;
+
+/**
+ * Created by Admin on 11/21/2017.
+ */
+
+public class slideImagePojo {
+    String image;
+
+    public slideImagePojo() {
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
